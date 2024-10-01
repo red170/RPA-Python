7a275d71c3c629c85560bfaf04085da2e659a58d
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-01 17:13:56 -0600

    Actualización de Archivo

--- 2. Tipos de Datos.py
+++ 2. Tipos de Datos.py
@@ -4,7 +4,7 @@
 numero_entero = 10
 print(numero_entero)  # Salida: 10
 
-# Números de punto flotante
+# Números de punto Flotante (Decimales)
 numero_flotante = 10.5
 print(numero_flotante)  # Salida: 10.5
 
@@ -17,3 +17,9 @@ es_verdadero = True
 es_falso = False
 print(es_verdadero)  # Salida: True
 print(es_falso)      # Salida: False
+
+#Verificar el Tipo de Dato
+print(type(numero_entero))
+print(type(numero_flotante))
+print(type(cadena_texto))
+print(type(es_falso))
