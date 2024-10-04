e7b288d635ed3f5fc35f2faebd413fec08945d64
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-04 16:51:06 -0600

    Actualizacion de Archivos

---
+++ 6 Metodos de Strings.py
@@ -0,0 +1,59 @@
+#Metodos de Cadenas
+
+
+#Variable a Manejar
+cadena1 = 'Hola soy ernesto'
+
+
+#Cambiar texto a Mayusculas
+upper = cadena1.upper()
+print(upper)
+
+#Convertir texto a Minusculas
+lower = cadena1.lower()
+print(lower)
+
+#Convertir la primera letra a Mayusculas
+capitalize = cadena1.capitalize()
+print(capitalize)
+
+#Buscar una Cadena dentro de otra y devuelve el index de donde empieza el trozo de cadena buscado (al exixtir un error devuelve -1)
+find = cadena1.find('a')
+print(find)
+
+#Buscar una cadena dentro de otra, igual que el anterior pero al no encontrar la busqueda devuleve un error y no imprime '-1'
+index = cadena1.index('a')
+print(index)
+
+#Si es numerico devuelve True sino False
+isnumeric = cadena1.isnumeric()
+print(isnumeric)
+
+#Si es alfanumerico devuelve True sino False (Valores de la 'A' la 'Z' sin valorar espacios)
+isalpha = cadena1.isalpha()
+print(isalpha)
+
+#Buscar una cadena dentro de otra, y devulve la cantidad de  coincidencias encontradas
+count = cadena1.count('a')
+print(count)
+
+#Devuelve el largo de una cadena
+largo = len(cadena1)
+print(largo)
+
+#Verificar si una cadena empieza con otra
+start = cadena1.startswith('Hola')
+print(start)
+
+#Verificar si una cadena termina con otra
+end = cadena1.endswith('hola')
+print(end)
+
+#Reemplazar un trozo de la cadena por otro
+replace = cadena1.replace('Hola', 'Hola soy')
+print(replace)
+
+#Separar cadenas con la cadena que le indiquemos y devuleve una lista
+split = cadena1.split(' ')
+print(split)
+print(split[1])
--- Metodos.py
+++
@@ -1,6 +0,0 @@
-#Metoodos de Cadenas
-
-cadena1 = 'Hola soy ernesto'
-cadena2 = 'que pex'
-
-print(dir(cadena1))
