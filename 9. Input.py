9b73681cc0235d207b2bcada4c2bca36a11ae540
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-05 16:10:39 -0600

    Actualizacion de Archivos

---
+++ 9. Input.py
@@ -0,0 +1,18 @@
+#///Input de Nombres
+
+#Pedirle un Dato al Usuario
+nombre = input('Cual es tu nombre? ')
+
+#Mostrar el Nombre
+print(f'Hola {nombre}')
+
+#///Input de Numeros
+numero = input('ingresa un Numero ')
+
+#Convertir el Dato a Numero y Operarlo
+resultado = int(numero) * 3
+# resultado = float(numero) * 3
+
+#Mostar el Numero
+print(numero)
+
