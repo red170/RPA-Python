3c469f04d5acb67f914fd69bafcfa3ccf3944a83
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-07 15:27:16 -0600

    Actualizacion de Archivos

---
+++ 8. Desempaquetado.py
@@ -0,0 +1,11 @@
+# Sirve para extraer los datos de un arreglo y ubicarlos en variables idependientes funciona tanto para listas como  para tuplas
+datos = ('Neto', 'Portillo')
+
+
+#Desesmpaquetado
+nombre,apellidos = datos
+
+print(nombre)
+print(apellidos)
+
+
