6b43106adc3c4154e1279f4819d598cd4da341ef
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-10 17:43:56 -0600

    Actualizacion de Archivos

--- 13. Modulos.py
+++ 13. Modulos.py
@@ -13,11 +13,25 @@ Modulos Propios
 # saludo = sal.saludar("Oscar")
 # print(saludo)
 
-#Importar una Funcion especifica de un Modulo
-from zmodulo import saludar
+# #Importar una Funcion especifica de un Modulo
+# from zmodulo import saludar
+# saludo = saludar('oscar')
+# print(saludo)
+
+#Importar Variable
+# from zmodulo import salu2
+# print(salu2)
+
+#Importar una Funcion especifica de un Modulo  que esta dentro de una Carpeta fuera de donde esta el archivo
+# import sys
+# sys.path.append('ruta_de_la_carpeta_contenedora')
+# import nombre_del_modulo
+
+#Importar una Funcion especifica de un Modulo  que esta dentro de una Carpeta
+from Modulos.zmodulo import saludar
 saludo = saludar('oscar')
 print(saludo)
 
-#Importar Variable
-from zmodulo import salu2
+#Importar Variable que esta dentro de una Carpeta
+from Modulos.zmodulo import salu2
 print(salu2)
