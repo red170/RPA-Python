7600ab4212b838f430a03d0f49cb65e224478585
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-11 16:09:21 -0600

    Carga de Archivos

---
+++ 14. Paquetes.py
@@ -0,0 +1,4 @@
+#Los Paquqtes son un cojunto de Modulos los cuales sirven para almacenar todos los modulos en una carpeta y cargarlos todos de una vez
+import Modulos.zmodulo
+
+print(Modulos.zmodulo.salu2)
