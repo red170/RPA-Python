85996f6409cb761fc5b74f20c02f0e2befa39c76
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-10-14 16:10:39 -0600

    Actualizacion de Archivos

--- 17.1. Leer CSV.py
+++ 17.1. Leer CSV.py
@@ -1,4 +1,9 @@
 import csv
 
 with open ("./Resources/test.csv") as archivo:
-    print(archivo.read())
+    # print(archivo.read())
+    
+    #Imprimir con Formato
+    reader = csv.reader(archivo)
+    for row in reader:
+        print(row)
