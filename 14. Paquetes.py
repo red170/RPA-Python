61d9db113336cec7d5a3af261d99875f0463dbcc
Ernesto Portillo <ernestoportillo170@gmail.com>
2024-10-28 17:47:11 -0600

    Reestructuracion de Contenidos

--- 14. Paquetes.py
+++
@@ -1,4 +0,0 @@
-#Los Paquqtes son un cojunto de Modulos los cuales sirven para almacenar todos los modulos en una carpeta y cargarlos todos de una vez
-import Modulos.zmodulo
-
-print(Modulos.zmodulo.salu2)
